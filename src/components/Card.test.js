16ce6f7341fff1d75e745fd0e04c8911b549e77d
Andrés Muñoz <asmz5488@gmail.com>
2019-05-26 10:49:03 -0500

    card test edit and adding prettier

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import { shallow, mount, render } from "enzyme";
-import  Card  from "./Card"
+import Card from "./Card";
 
-describe('Card', () => {
-  it('', () => {
-    expect(shallow(<Card />).length).toBe(1)
-    expect(shallow(<Card />).contains()).toEqual(1)
-  })
-})
+describe("Card", () => {
+  it("", () => {
+    expect(shallow(<Card />).length).toBe(1);
+  });
+});
