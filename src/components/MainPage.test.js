c2bb9567cecb1057658cb6cd0bbb90010295152b
_andy_mz <asmz5488@gmail.com>
2019-06-10 10:10:50 -0500

    making test on jest

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -4,24 +4,46 @@ import MainPage from "./MainPage";
 
 describe("MainPage", () => {
   let wrapper;
+  let mainPageWrapper;
 
   beforeEach(() => {
-    const mockProps = {
-      robots: [],
-      searchField: "",
-      onRequestRobots: jest.fn(),
-      onSearchChange: jest.fn(),
-      isPending: false
-    };
+    mainPageWrapper = (searchField = "", robots = [], isPending = false) => {
+      const mockProps = {
+        searchField,
+        robots,
+        onRequestRobots: jest.fn(),
+        onSearchChange: jest.fn(),
+        isPending
+      };
 
-    wrapper = shallow(<MainPage {...mockProps} />);
+      return shallow(<MainPage {...mockProps} />);
+    };
   });
 
-  it("render MainPage without crashing", () => {
+  it("should render MainPage without crashing", () => {
+    wrapper = mainPageWrapper();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should render MainPage loading when isPending is true", () => {
+    wrapper = mainPageWrapper("", [], true);
     expect(wrapper).toMatchSnapshot();
   });
 
   it("should filter robots", () => {
-    expect(wrapper).toMatchSnapshot();
+    wrapper = mainPageWrapper();
+    expect(wrapper.instance().filteredRobots()).toEqual([]);
+
+    wrapper = mainPageWrapper("steve", [{ name: "joe" }, { name: "lisa" }]);
+    expect(wrapper.instance().filteredRobots()).toEqual([]);
+
+    wrapper = mainPageWrapper("joe", [{ name: "joe" }, { name: "lisa" }]);
+    expect(wrapper.instance().filteredRobots()).toEqual([{ name: "joe" }]);
+
+    wrapper = mainPageWrapper("a", [{ name: "andy" }, { name: "alf" }]);
+    expect(wrapper.instance().filteredRobots()).toEqual([
+      { name: "andy" },
+      { name: "alf" }
+    ]);
   });
 });
